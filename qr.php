b27ead53948cd835ea1f0dce6bfdf6af2f5a5d84
My1 <My1@users.noreply.github.com>
2017-05-20 04:16:19 +0200

    Added QR Code signin
    Big Thanks to Primeauth Staff for testing.

---
+++ qr.php
@@ -0,0 +1,77 @@
+<?php
+$apptok = ""; //token of the application
+$appsec = ""; //secret of the application
+
+if(!isset($_GET["try"])) {
+  $apiurl="https://api.primeauth.com/api/v1/pascan/gen.json";
+  
+  $adata=array(
+    'token' => $apptok,
+    'secret' => $appsec
+  );
+  
+  $curl=curl_init($apiurl);
+  curl_setopt_array($curl,array(
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_HTTPHEADER=> array('User-Agent: Mugen Auth Beta by My1'),
+    CURLOPT_POST=> true,
+    CURLOPT_POSTFIELDS => http_build_query($adata)));
+  
+  $response=curl_exec($curl);
+  $cstat=curl_errno($curl);
+  $status=curl_getinfo($curl,CURLINFO_HTTP_CODE);
+  if($cstat==0) {
+    var_dump ($status);
+    $data=json_decode($response,true);
+    echo "<pre>";var_dump ($data);echo "</pre>";
+    if($data["id"]) {
+      echo "<img src='https://api.primeauth.com/pascan/qrcode?id=".$data["id"]."'>";
+      echo "<a href=?try=".$data["id"].">Login</a>";
+    }
+  }
+  else{ //curl screwed up. log the error and return false
+    $log=fopen("opa-log.txt","ab");
+    fwrite ($log,"PA Login cURL Error (step1): $cstat / ".curl_error($curl)."\n");
+    fclose($log);
+    die();
+  }
+  die();
+}
+
+
+elseif($_GET["try"]) {
+  $apiurl="https://api.primeauth.com/api/v1/pascan/check.json";
+  
+  $adata=array(
+    'id' => $_GET["try"],
+    'token' => $apptok,
+    'secret' => $appsec
+  );
+
+  $curl=curl_init($apiurl);
+  curl_setopt_array($curl,array(
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_HTTPHEADER=> array('User-Agent: Mugen Auth Beta by My1'),
+    CURLOPT_POST=> true,
+    CURLOPT_POSTFIELDS => http_build_query($adata)));
+  
+  $response=curl_exec($curl);
+  $cstat=curl_errno($curl);
+  $status=curl_getinfo($curl,CURLINFO_HTTP_CODE);
+  if($cstat==0) {
+    var_dump ($status);
+    $data=json_decode($response,true);
+    echo "<pre>";var_dump ($data);echo "</pre>";
+    if($data["accepted"]==1) {
+      echo "Login complete for ".$data["email"];
+    }
+  }
+  else{ //curl screwed up. log the error and return false
+    $log=fopen("opa-log.txt","ab");
+    fwrite ($log,"PA Login cURL Error (step2): $cstat / ".curl_error($curl)."\n");
+    fclose($log);
+    die();
+  }
+  die();
+}
+?>
